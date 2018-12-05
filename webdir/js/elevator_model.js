8839b5c992da0498eaefdab234ed85aa23851d1a
MatthewNewburke <TheNewburkeWork@gmail.com>
2018-12-04 22:49:53 -0700

    More functions

--- webdir/js/elevator_model.js
+++ webdir/js/elevator_model.js
@@ -1,24 +1,36 @@
 
 // Elevator will have a controller that will construct and control the simulation
 
-import("./owl-deepcopy");
+//import("./owl-deepcopy");
 
 ElevatorController = {
 	//Properties
 	cars: [],
-
-
+	floors: 1,
 	//Methods
 	init: function(numberOfCars, numberOfFloors) {
-		console.log("params are ", numberOfCars, " and ", numberOfFloors);
 		this.cars = [];
+		if (numberOfFloors == null || < 1) { this.floors = 1; }
+		else { this.floors = numberOfFloors; }
+		
 		for (var i=0; i<numberOfCars; i++)  {
 			this.cars.push(this.createCar());
 		}
 	},
 	createCar: function() {
-		return deepCopy(Car);
+		return Car.init();
+		//return deepCopy(Car);
 	},
+	getTopFloor: function() {
+		return this.floors;
+	},
+	//Car calling methods
+	requestCar: function() {
+
+	},
+
+
+
 };
 
 Car = {
@@ -28,12 +40,32 @@ Car = {
 	occupied: false,
 
 	init: function() {
-
+		return this;
 		//return a car object
+		//NOTE: if this singleton issue is present owl.deepCopy addresses it
 	},
+	reportFloor: function() {
+
+	},
+	reportOpening: function() {
+
+	},
+	reportClosing: function() {
+
+	},
+	setCurrentFloor: function(floor) {
+		if (floor > ElevatorController.getTopFloor()) { this.currentFloor = ElevatorController.getTopFloor(); }
+		if (floor < 1) { this.currentFloor = 1; }
+		this.currentFloor = floor;
+	},
+
 };
 
 ElevatorController.init(2,2);
+
+
+
+
 
 let d = new Date();
 document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
