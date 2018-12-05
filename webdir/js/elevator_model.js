7c42de2814122fe872fc3dffda0ce50937752878
MatthewNewburke <TheNewburkeWork@gmail.com>
2018-12-04 22:13:43 -0700

    Update elevator_model.js
    Starting to build out ElevatorContoller and other elements

--- webdir/js/elevator_model.js
+++ webdir/js/elevator_model.js
@@ -1,10 +1,34 @@
 
+// Elevator will have a controller that will construct and control the simulation
+
+
+ElevatorController = {
+	//Properties
+	cars: [],
+
+
+	//Methods
+	init: function(numberOfCars, numberOfFloors) {
+		console.log("params are ", numberOfCars, " and ", numberOfFloors);
+		this.cars = [];
+		for (var i=0; i<numberOfCars; i++)  {
+			this.cars.push(this.createCar());
+		}
+	},
+	createCar: function() {
+		return Car.init();
+	},
+};
+
+Car = {
+	init: function() {
+
+		//return a car object
+	},
+};
+
+ElevatorController.init(2,2);
+
 let d = new Date();
 document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
 
-
-
-
-
-
-//TEST commit to set the timer
